4753b2ba16256aeca61c09f521e2ed47a8aa62c5
solutionnuts <64245128+solutionnuts@users.noreply.github.com>
2020-07-09 15:26:55 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
 html_logo = './_images/EmComMap_Logo.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
